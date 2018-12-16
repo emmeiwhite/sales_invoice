e3ce158fa34eb59a32ded2b3b15edd1f87d842f0
emmeiwhite <emmeiwhite@gmail.com>
2018-12-16 20:55:43 +0530

    DB is up and running, I am creating Front End layout

--- sales_invoice.php
+++ sales_invoice.php
@@ -4,9 +4,46 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+
     <title>Sales Invoice | Imran Rafiq Rather</title>
   </head>
   <body>
-      <h1>Imran Rafiq Rather</h1>
+
+  <section class="container-fluid">
+    <header class="header-section">
+        <h1 align="center">INVOICE LIST</h1>
+    </header>
+    
+    <main>
+      <table id="dataTable" class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>Invoice Number </th>  
+              <th>Invoice Date</th>  
+              <th>Receiver Name</th>  
+              <th>Invoice Total</th>  
+              <th>PDF</th>  
+              <th>EDIT</th>  
+              <th>DELETE</th>  
+            </tr>
+          </thead>
+      </table>
+    </main>
+
+    
+  </section>
+
+      
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
